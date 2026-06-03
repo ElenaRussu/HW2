bf5c1f13d4582135fd0e6ea56fa8c74bac281c2c
ElenaRussu <lenarussu07@gmail.com>
2026-06-03 12:53:29 +0300

    исправила ошибки в методах

--- main/ShoppingList.py
+++ main/ShoppingList.py
@@ -1,23 +1,26 @@
 from Ingredient import Ingredient
 from Recipe import Recipe
 
+
 class ShoppingList:
+
     def __init__(self):
         self._items = []
 
-def add_recipe(self, recipe: Recipe, portions: float):
+    def add_recipe(self, recipe: Recipe, portions: float):
         if portions <= 0:
             raise ValueError("Количество порций должно быть положительным")
         scaled = recipe.scale(portions)
         for ingredient in scaled.ingredients:
             self._items.append((ingredient, recipe.title))
 
-def remove_recipe(self, title: str):
+    def remove_recipe(self, title: str):
         self._items = [
             (ing, t) for (ing, t) in self._items
             if t != title
         ]
-def get_list(self) -> list:
+
+    def get_list(self) -> list:
         totals = {}
         for ingredient, _ in self._items:
             key = (ingredient.name, ingredient.unit)
@@ -31,7 +34,7 @@ def get_list(self) -> list:
         ]
         return sorted(result, key=lambda ing: ing.name)
 
-def __add__(self, other):
-    new_list = ShoppingList()
-    new_list._items = self._items + other._items
-    return new_list
+    def __add__(self, other):
+        new_list = ShoppingList()
+        new_list._items = self._items + other._items
+        return new_list
